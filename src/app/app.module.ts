7b87bb8651ca33ee4ea6cac95ebc483c6e7e0527
activexJ <activex12@gmail.com>
2017-10-18 00:07:00 +0530

    added dashboard partial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule }   from '@angular/forms';
+import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
-import {HeroDetailComponent} from './hero-detail.component';
-import {HeroService} from './hero.service'
+import { HeroDetailComponent } from './hero-detail.component';
+import { HeroesComponent } from './heroes.component';
+import { DashboardComponent } from './dashboard.component';
+import { HeroService } from './hero.service';
+import { RouterModule } from '@angular/router';
+import {AppRoutingModule} from './app-routing.module'
+
 
 
 @NgModule({
   declarations: [
     AppComponent,
     HeroDetailComponent,
+    HeroesComponent,
+    DashboardComponent
+
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    AppRoutingModule
   ],
   providers: [HeroService],
   bootstrap: [AppComponent]
